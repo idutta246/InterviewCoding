0ef4889002071ddf7a9074ded81968f95d755cb8
idutta246 <indranildutta246@gmail.com>
2023-01-12 16:18:25 +0530

    Revert "Interview Coding for Order and Items"
    This reverts commit 7fab60ad6c79646087539fd02240cd39da455f28.

--- src/main/java/com/interview_coding/orderapi/dao/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.interview_coding.orderapi.dao;
-
-import com.interview_coding.orderapi.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-}
